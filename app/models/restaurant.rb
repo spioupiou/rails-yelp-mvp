c2810c0a99e9c5df8073783fbd50feec0693ae42
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-13 18:55:01 +0900

    Solved the JS issue

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: ["Chinese", "Italian", "Japanese", "French", "Belgian"] }
+end
