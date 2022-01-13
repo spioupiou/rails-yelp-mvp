bc084efda7f4c1429e473e31eaa0f6a9d3e0baaf
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-13 19:32:44 +0900

    Solved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_restaurant
 
-  def new
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
